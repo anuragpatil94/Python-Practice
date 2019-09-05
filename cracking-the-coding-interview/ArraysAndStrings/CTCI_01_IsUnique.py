791545f20b72dbfa5384fcd2f9300e7db484252f
Anurag Patil <aspanurag@gmail.com>
2019-09-05 11:38:21 -0400

    CTCI|ArraysAndStrings| IsUnique|1.1

--- cracking-the-coding-interview/ArraysAndStrings/CTCI_01_IsUnique.py
+++ cracking-the-coding-interview/ArraysAndStrings/CTCI_01_IsUnique.py
@@ -1,6 +1,26 @@
 '''
+Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+What if you cannot use additional data structures? 
 
+My Solution With additional data structure
+- using set to check if a duplicate character exists
 '''
 class Solution:
     def isUnique(string):
-        return string   
+        '''
+        - Book Solution
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        '''
+        vector = 0
+        for char in string:
+            charInt = ord(char)
+            if(vector & 1<<charInt > 0):
+                return False    
+            vector |= 1<<charInt
+            print(bin(vector))
+        return True
+
+if(__name__=="__main__"):
+    s = Solution
+    Solution.isUnique("abce")
--- cracking-the-coding-interview/ArraysAndStrings/test_IsUnique.py
+++ cracking-the-coding-interview/ArraysAndStrings/test_IsUnique.py
@@ -1,3 +1,33 @@
 from CTCI_01_IsUnique import Solution
-def test_string():
-    assert Solution.isUnique("this") == "this"
+
+def test_EmptyString():
+    string = ""
+    assert Solution.isUnique(string) == True, "Has Duplicate Characters"
+
+def test_AllUnique():
+    string = "abcdefghijklmnopqrstuvwxyz"
+    assert Solution.isUnique(string) == True, "Has Duplicate Characters"
+
+def test_Only1Char():
+    string = "a"
+    assert Solution.isUnique(string) == True, "Has Duplicate Characters"
+
+def test_Only2Char():
+    string = "ab"
+    assert Solution.isUnique(string) == True, "Has Duplicate Characters"
+
+def test_Only2SameChar():
+    string = "aa"
+    assert Solution.isUnique(string) == False
+
+def test_SameChars():
+    string = "aabbccdd"
+    assert Solution.isUnique(string) == False
+
+def test_SameCharsInTheEnd():
+    string = "abcdefff"
+    assert Solution.isUnique(string) == False
+
+def test_SameCharInRandomPos():
+    string = "agndheyjsmoplqg"
+    assert Solution.isUnique(string) == False
