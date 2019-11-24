3ccf425437fcd48d9df2b0d3499800c0e33f614c
Anurag Patil <aspanurag@gmail.com>
2019-11-24 07:05:10 -0500

    DFS and BFS

--- TopicWise/TreesAndGraph/BFS.py
+++ TopicWise/TreesAndGraph/BFS.py
@@ -1,6 +1,7 @@
 from AdjacencyList import AdjacencyList
 import json
 
+
 class BFS:
     def BFS(self, source, list: AdjacencyList):
         level = {source: 0}
@@ -18,10 +19,11 @@ class BFS:
                         next.append(current.val)
                     current = current.next
             frontier = next
-            levelNumber+=1
-        return level,parent
+            levelNumber += 1
+        return level, parent
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     list = AdjacencyList()
     list.addVertex("a")
     list.addVertex("z")
@@ -31,27 +33,27 @@ if __name__=="__main__":
     list.addVertex("c")
     list.addVertex("f")
     list.addVertex("v")
-    list.addEdge("a","z")
-    list.addEdge("a","s")
-    list.addEdge("s","x")
-    list.addEdge("x","d")
-    list.addEdge("x","c")
-    list.addEdge("c","d")
-    list.addEdge("c","f")
-    list.addEdge("c","v")
-    list.addEdge("f","v")
-    list.addEdge("z","a")
-    list.addEdge("s","a")
-    list.addEdge("x","s")
-    list.addEdge("d","x")
-    list.addEdge("c","x")
-    list.addEdge("d","c")
-    list.addEdge("f","c")
-    list.addEdge("v","c")
-    list.addEdge("v","f")
+    list.addEdge("a", "z")
+    list.addEdge("a", "s")
+    list.addEdge("s", "x")
+    list.addEdge("x", "d")
+    list.addEdge("x", "c")
+    list.addEdge("c", "d")
+    list.addEdge("c", "f")
+    list.addEdge("c", "v")
+    list.addEdge("f", "v")
+    list.addEdge("z", "a")
+    list.addEdge("s", "a")
+    list.addEdge("x", "s")
+    list.addEdge("d", "x")
+    list.addEdge("c", "x")
+    list.addEdge("d", "c")
+    list.addEdge("f", "c")
+    list.addEdge("v", "c")
+    list.addEdge("v", "f")
     print(list.print_graph())
     print("--")
     bfs = BFS()
-    level,parent = bfs.BFS("s",list)
-    print(json.dumps(level,indent=4))
-    print(json.dumps(parent,indent=4))
+    level, parent = bfs.BFS("s", list)
+    print(json.dumps(level, indent=4))
+    print(json.dumps(parent, indent=4))
--- TopicWise/TreesAndGraph/DFS.py
+++ TopicWise/TreesAndGraph/DFS.py
@@ -1,25 +1,27 @@
 from AdjacencyList import AdjacencyList
 import json
 
+
 class DFS:
-    def DFS_Visit(self, source,list,parent):
+    def DFS_Visit(self, source, list, parent):
         current = list.getVertex(source)
-        print(current,current.val)
         while current is not None:
             # print(current,current.val)
             if current.val not in parent:
                 parent[current.val] = source
-                self.DFS_Visit(current.val,list,parent)
-            current=current.next
+                self.DFS_Visit(current.val, list, parent)
+            current = current.next
 
     def DFS(self, list):
         parent = {}
         for vertex in list:
             if vertex.val not in parent:
                 parent[vertex.val] = None
-                self.DFS_Visit(vertex.val,list,parent)
+                self.DFS_Visit(vertex.val, list, parent)
         return parent
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     list = AdjacencyList()
     list.addVertex("a")
     list.addVertex("b")
@@ -27,17 +29,17 @@ if __name__=="__main__":
     list.addVertex("d")
     list.addVertex("e")
     list.addVertex("f")
-    list.addEdge("a","b")
-    list.addEdge("a","d")
-    list.addEdge("b","e")
-    list.addEdge("d","b")
-    list.addEdge("e","d")
-    list.addEdge("c","e")
-    list.addEdge("c","f")
-    list.addEdge("f","f")
+    list.addEdge("a", "b")
+    list.addEdge("a", "d")
+    list.addEdge("b", "e")
+    list.addEdge("d", "b")
+    list.addEdge("e", "d")
+    list.addEdge("c", "e")
+    list.addEdge("c", "f")
+    list.addEdge("f", "f")
     print(list.print_graph())
     print("--")
     dfs = DFS()
     # DFS_Visit("a",list)
     parent = dfs.DFS(list)
-    print(json.dumps(parent,indent=4))
+    print(json.dumps(parent, indent=4))
