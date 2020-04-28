c6672c1c49de53d8b57d6d2f2bcaca91da0243cd
= <=>
2020-04-28 13:40:47 -0400

    Another way to track visited

--- OnlineAssessmentQuestions/N13_TreasureIsland.py
+++ OnlineAssessmentQuestions/N13_TreasureIsland.py
@@ -100,6 +100,29 @@ class Solution:
         return path
         pass
 
+    def solution3(self, m):
+        ''' Time (m)  Space (m) '''
+        if len(m) == 0 or len(m[0]) == 0:
+            return -1  # impossible
+
+        matrix = [row[:] for row in m]
+        nrow, ncol = len(matrix), len(matrix[0])
+
+        q = deque([((0, 0), 0)])  # ((x, y), step)
+        matrix[0][0] = "D"
+        while q:
+            (x, y), step = q.popleft()
+
+            for dx, dy in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                if 0 <= x+dx < nrow and 0 <= y+dy < ncol:
+                    if matrix[x+dx][y+dy] == "X":
+                        return step+1
+                    elif matrix[x+dx][y+dy] == "O":
+                        # mark visited
+                        matrix[x + dx][y + dy] = "D"
+                        q.append(((x+dx, y+dy), step+1))
+
+        return -1
 
 if __name__ == "__main__":
     s = Solution()
